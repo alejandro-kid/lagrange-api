3657894ecfd39839f16b50474557f1d9415c95ce
Alejandro Roble Gutierrez <arobleg@gmail.com>
2023-08-23 17:21:11 -0400

    Implement test for lagrange-endpoint

--- test/lagrange.test.js
+++ test/lagrange.test.js
@@ -3,19 +3,15 @@ const app = require("../app");
 
 describe("POST /lagrange", () => {
     it("test lagrange endpoint", async () => {
-      const res = await request(app).post("/lagrange").send({
-        number: 65
-      });
-      expect(res.statusCode).toBe(200);
-      expect(reverse_lagrange(res.body.lagrange)).toBe(65);
+        const res = await request(app).post("/lagrange").send({
+            number: 60
+        });
+        expect(res.statusCode).toBe(200);
+        expect(res.body.lagrange.length).toBe(4);
+        expect(reverse_lagrange(res.body.lagrange)).toBe(60);
     });
   });
 
-const reverse_lagrange = (n) =>{
-    if(n.length !==4){
-        return false;
-    }
-    else{
-        return n[0] + n[1] + n[2] + n[3]
-    }
+  const reverse_lagrange = (n) => {
+    return n[0] ** 2 + n[1] ** 2 + n[2] ** 2 + n[3] ** 2;
 }
